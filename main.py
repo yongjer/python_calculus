c3f34f822bfcd3b5548b7a6255b93e5bf3c16859
yongjer <54315206+yongjer@users.noreply.github.com>
2022-07-11 13:29:38 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+input_question = input("enter the question: ")
+input_question = input_question.split(" ")
+chromedriver = "/Users/yongjer/Downloads/chromedriver"
+option = webdriver.ChromeOptions()
+option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+s = Service(chromedriver)
+driver = webdriver.Chrome(service=s, options=option)
+driver.get(f"https://www.calcchat.com/book/Calculus-11e/{input_question[0]}/{input_question[1]}/{input_question[2]}/")
